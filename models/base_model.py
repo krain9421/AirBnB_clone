3e316e793e78e031be1787881db42fa917993166
Nwigwe Chidubem <krain9421@gmail.com>
2023-02-08 10:20:46 +0000

    commit

--- models/base_model.py
+++ models/base_model.py
@@ -7,15 +7,18 @@ from datetime import datetime
 class BaseModel:
     """BaseModel class
 
-    Fields:
+    Attributes:
         id (string): The unique id of the object
         created_at (datetime): The time when an instance is created
         updated_at (datetime): The time to be updated when an object
                                 is changed
     """
-    id = str(uuid.uuid4())
-    created_at = datetime.today()
-    updated_at = created_at
+
+    def __init__(self):
+        """Initializes the BaseModel"""
+        self.id = str(uuid.uuid4())
+        self.created_at = datetime.today()
+        self.updated_at = self.created_at
 
     def save(self):
         """Updates the public instance attribute `updated_at` with
@@ -24,4 +27,17 @@ class BaseModel:
         self.updated_at = datetime.now()
 
     def __str__(self):
-        """Return the printable representation of the BaseModel."""
+        """Returns the printable representation of the BaseModel."""
+        repr = "[BaseModel] " + "(" + self.id + ") "
+        repr += str(self.__dict__)
+        return (repr)
+
+    def to_dict(self):
+        """Returns a dictionary containing all key/values of __dict__
+            of the instance
+        """
+        dict = self.__dict__.copy()
+        dict.update(__class__="BaseModel")
+        dict.update(created_at=self.created_at.isoformat())
+        dict.update(updated_at=self.updated_at.isoformat())
+        return (dict)
