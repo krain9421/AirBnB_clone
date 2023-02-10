895bf7484d21dbc9708f29cd4bb599de567214f9
Nwigwe Chidubem <krain9421@gmail.com>
2023-02-10 12:13:56 +0000

    updated console.py

--- console.py
+++ console.py
@@ -3,6 +3,13 @@
     interpreter
 """
 import cmd
+import inspect
+from models.base_model import BaseModel
+from models import storage
+import sys
+current_module = sys.modules[__name__]
+# Dictionary that will contain all the classes
+class_dict = dict(inspect.getmembers(current_module))
 
 
 class HBNBCommand(cmd.Cmd):
@@ -20,6 +27,70 @@ class HBNBCommand(cmd.Cmd):
 
     def emptyline(self):
         """Do nothing if an empty line is passed"""
+
+    def do_create(self, name):
+        """Creates a new instance of BaseModel,
+            saves it to the JSON file and prints
+            the id.
+        """
+        # Dictionary that will contain all the classes
+        # class_dict = dict(inspect.getmembers(current_module))
+        if not name:
+            print("** class name missing **")
+        elif name not in class_dict.keys():
+            print("** class doesn't exist **")
+        else:
+            new = class_dict[name]()
+            new.save()
+            print(new.id)
+
+    def do_show(self, line):
+        """Prints the string representation of an
+            instance based on class name and id
+        """
+        lines = line.split()
+        try:
+            name = lines[0]
+        except IndexError:
+            name = None
+
+        try:
+            idi = lines[1]
+        except IndexError:
+            idi = None
+
+        if not name:
+            print("** class name missing **")
+        elif name not in class_dict.keys():
+            print("** class doesn't exist **")
+        elif not idi:
+            print("** instance id missing **")
+        else:
+            d = storage.all()
+            # Flag for checking if instance is found
+            flag = False
+            # print("File__objects\n{}".format(d))
+            for key in d.keys():
+                if idi == d[key]["id"]:
+                    print(d[key])
+                    flag = True
+            if not flag:
+                print("** no instance found **")
+
+    def do_destroy(self, line):
+        """Deletes an instance based on the class name
+            and id (saves to JSON file)
+        """
+        lines = line.split()
+        try:
+            name = lines[0]
+        except IndexError:
+            name = None
+
+        try:
+            idi = lines[1]
+        except IndexError:
+            idi = None
 
 
 if __name__ == '__main__':
