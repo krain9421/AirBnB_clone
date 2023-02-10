1b004dbb579e86b91ab17b941c36a7d8aaa42b77
Nwigwe Chidubem <krain9421@gmail.com>
2023-02-10 22:56:15 +0000

    commit

--- console.py
+++ console.py
@@ -12,6 +12,46 @@ current_module = sys.modules[__name__]
 class_dict = dict(inspect.getmembers(current_module))
 
 
+def idexist(idi=""):
+    """Helper function to check if instance of class
+        exists for the `id`.
+        Returns True or False.
+    """
+    d = storage.all()
+    flag = False
+    for key in d.keys():
+        if idi == d[key]["id"]:
+            flag = True
+            break
+    return (flag)
+
+
+def valuecast(value=""):
+    """Helper function to cast attribute value
+        to attribute type.
+        Returns the casted value.
+    """
+    digits = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"]
+    if value[0] == "\"" and value[value.len()-1] == "\"":
+        return value[1:-1]
+    elif value[0] in digits:
+        index = -1
+        for i in value:
+            index += 1
+            if i not in digits:
+                if i == ".":
+                    temp = value[index+1:]
+                    for j in temp:
+                        if j not in digits:
+                            return (value)
+                    return (float(value))
+                else:
+                    return (value)
+        return (int(value))
+    else:
+        return (value)
+
+
 class HBNBCommand(cmd.Cmd):
     """HBNB command processor"""
 
@@ -126,6 +166,57 @@ class HBNBCommand(cmd.Cmd):
             print(list_all)
         else:
             print("** class doesn't exist **")
+
+    def do_update(self, line):
+        """Updates an instance based on the class name
+            and `id` by adding or updating attribute
+            (save the changes into the JSON file)
+        """
+        lines = line.split()
+        try:
+            name = lines[0]
+        except IndexError:
+            name = None
+
+        try:
+            idi = lines[1]
+        except IndexError:
+            idi = None
+
+        try:
+            attr = lines[2]
+        except IndexError:
+            attr = None
+
+        try:
+            value = lines[3]
+        except IndexError:
+            value = None
+
+        if not name:
+            print("** class name missing **")
+        elif name not in class_dict.keys():
+            print("** class doesn't exist **")
+        elif not idi:
+            print("** instance id missing **")
+        elif not idexist(idi):
+            print("** no instance found **")
+        elif not attr:
+            print("** attribute name missing **")
+        elif not value:
+            print("** value missing **")
+        else:
+            d = storage.all()
+            flag = False
+            for key in d.keys():
+                if idi == d[key]["id"]:
+                    if value[0] == "\"":
+                        value = value[1:-1]
+                    else:
+                        value = valuecast(value)
+                    d[key][attr] = value
+                    storage.save()
+                    break
 
 
 if __name__ == '__main__':
