56b5be17b2f6c345b238507fd6ac5eda5c7e9a95
Nwigwe Chidubem <krain9421@gmail.com>
2023-02-12 17:24:05 +0000

    commit

--- models/user.py
+++ models/user.py
@@ -1,18 +1,16 @@
 #!/usr/bin/python3
-"""User module that inherits from BaseModel"""
+"""User module that inherits from a BaseModel."""
 from models.base_model import BaseModel
 
 
 class User(BaseModel):
     """
-        Defines a User class that inherits from
-        BaseModel
-
-        Public class attributes:
-        email(string): empty string
-        password(string): empty string
-        first_name(string): empty string
-        last_name(string): empty string
+    Defines a User class that inherits from
+    BaseModel with the following attributes.
+    + email(string): empty string
+    + password(string): empty string
+    + first_name(string): empty string
+    + last_name(string): empty string
     """
     email = ""
     password = ""
