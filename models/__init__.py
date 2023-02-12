beb3978f419d5a58e0801decba51416e95027e60
Nwigwe Chidubem <krain9421@gmail.com>
2023-02-12 03:54:14 +0000

    commit

--- models/__init__.py
+++ models/__init__.py
@@ -1,7 +1,9 @@
 #!/usr/bin/python3
-"""Initialization magic module for all the models"""
-from models.engine.file_storage import FileStorage
+"""Initialization magic module that
+    creates a FileStorage object for storage
+"""
 
+from models.engine.file_storage import FileStorage
 
 storage = FileStorage()
 storage.reload()
