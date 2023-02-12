66c98e98cd09e433b35ea9f9610399bf788cbb58
Nwigwe Chidubem <krain9421@gmail.com>
2023-02-12 15:55:23 +0000

    commit

--- tests/test_models/test_engine/test_file_storage.py
+++ tests/test_models/test_engine/test_file_storage.py
@@ -11,13 +11,14 @@ from uuid import uuid4
 class TestFileStorage(unittest.TestCase):
     """Defines unittest for file_storage module"""
 
-    def test_is_FileStorage(self):
-        """Tests to check if a FileModel instance
+    """def test_is_FileStorage(self):
+        Tests to check if a FileModel instance
             is created.
-        """
+        
         new = FileStorage()
         name = type(new).__name__
         self.assertEqual(name, "FileStorage")
+    """
 
     def test_storage_all(self):
         """Tests the return type of storage.all()
