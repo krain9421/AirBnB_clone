b6b0979996f4e8f50addf3b90f67c1d02a9c387a
Nwigwe Chidubem <krain9421@gmail.com>
2023-02-12 02:51:44 +0000

    commit

--- tests/test_models/test_engine/test_file_storage.py
+++ tests/test_models/test_engine/test_file_storage.py
@@ -1,3 +1,27 @@
 #!/usr/bin/python3
 """Unittests for file_storage module"""
 import unittest
+from models.engine.file_storage import FileStorage
+
+
+class TestFileStorage(unittest.TestCase):
+    """Defines unittest for file_storage module"""
+
+    def test_is_FileStorage(self):
+        """Tests to check if a FileModel instance
+            is created.
+        """
+        new = FileStorage()
+        name = type(new).__name__
+        self.assertEqual(name, "FileStorage")
+
+    def test_storage_all(self):
+        """Tests the return type of storage.all()
+            method.
+        """
+        new = FileStorage()
+        d = new.all()
+        self.assertEqual(type(d), dict)
+
+    if __name__ == '__main__':
+        unittest.main()
